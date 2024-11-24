eab1ac021f4ab53793a7c36530e32bc8f4109383
FIDHA FATHIMA <fidhalatheefm@gmail.com>
2024-11-24 23:02:49 +0530

    header design completed and home design started

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Social() {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/_f1.dha._/" className="home__social-icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/fidha-fathima-44809025a/" className="home__social-icon" target="_blank">
+            <i class="uil uil-linkedin-alt"></i>
+            </a>
+            <a href="https://github.com/FidhaLatheef" className="home__social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
