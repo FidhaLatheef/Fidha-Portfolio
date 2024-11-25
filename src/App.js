93808076277a792607835b950ef5d3ef75d6c7b3
FIDHA FATHIMA <fidhalatheefm@gmail.com>
2024-11-25 17:16:43 +0530

    home section design completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 
+
 const App=()=>{
+  
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }, []);
   return (
     <>
     <Header/>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import "./home.css"
 import Social from './Social'
 import Data from './Data'
+import ScrollDown from './ScrollDown'
 
 function Home() {
   return (
     <section className="home section" id="home">
-        <div className="home__container container grid">
+        <div className="home__container container grid" data-aos="fade-up">
             <div className="home__content grid">
                 <Social/>
                 
@@ -14,6 +15,8 @@ function Home() {
 
                 <Data/>
             </div>
+            
+            <ScrollDown/>
         </div>
     </section>
   )
