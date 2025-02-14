966b654187413087e29e1c83962a6ba016f04cee
FIDHA FATHIMA <fidhalatheefm@gmail.com>
2025-02-14 14:41:18 +0530

    responsive correction in about section fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,18 +3,23 @@ import "./home.css"
 import Social from './Social'
 import Data from './Data'
 import ScrollDown from './ScrollDown'
+import { motion } from "framer-motion";
 
 function Home() {
   return (
     <section className="home section" id="home">
-        <div className="home__container container grid" data-aos="fade-down">
-            <div className="home__content grid">
-                <Social/>              
-                <div className="home__img"></div>
-                <Data/>
-            </div>
-            <ScrollDown/>
+      <motion.div
+        className="home__container container grid"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}>
+        <div className="home__content grid">
+          <Social />
+          <div className="home__img"></div>
+          <Data />
         </div>
+        <ScrollDown />
+      </motion.div>
     </section>
   )
 }
